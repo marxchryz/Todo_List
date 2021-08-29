e099903ce633aceac8921a2619e97b4ac5879f8c
Marx Chryz <marxchryz30@gmail.com>
2021-08-29 18:33:12 +0800

    delete task functionality added

--- App.js
+++ App.js
@@ -21,6 +21,12 @@ export default function App() {
     setTask(null);
   };
 
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
       {/*Todays Tasks*/}
@@ -29,7 +35,11 @@ export default function App() {
         <View style={styles.items}>
           {/*This is where the tasks will go*/}
           {taskItems.map((item, index) => {
-            return <Task key={index} text={item} />;
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
           })}
         </View>
       </View>

070430699ff1f2f0f6bf1d2ce5d9bbbf77895bdc
Marx Chryz <marxchryz30@gmail.com>
2021-08-29 17:36:37 +0800

    props passsed to task component

--- App.js
+++ App.js
@@ -11,11 +11,8 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
         <View style={styles.items}>
           {/*This is where the tasks will go*/}
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
         </View>
       </View>
     </View>
